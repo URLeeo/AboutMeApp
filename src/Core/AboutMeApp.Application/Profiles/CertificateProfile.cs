eb3217224f4327d2b64341722a4f526499994e3d
URLeeo <mammadzadaaslan05@gmail.com>
2025-03-03 01:54:24 +0400

    AboutMeApp-4/added certificate controller, modified certificate service

---
+++ src/Core/AboutMeApp.Application/Profiles/CertificateProfile.cs
@@ -0,0 +1,17 @@
+﻿using AboutMeApp.Application.Dtos.Certificate;
+using AboutMeApp.Domain.Entities;
+using AutoMapper;
+
+namespace AboutMeApp.Application.Profiles;
+
+public class CertificateProfile : Profile
+{
+    public CertificateProfile()
+    {
+        CreateMap<Certificate, CertificateGetDto>()
+            .ForMember(dest => dest.ExpiryDate, opt => opt.MapFrom(src => src.ExpiryDate))
+            .ReverseMap();
+        CreateMap<Certificate, CertificateCreateDto>().ReverseMap();
+        CreateMap<Certificate, CertificateUpdateDto>().ReverseMap();
+    }
+}

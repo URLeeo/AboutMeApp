3208e6b3b0c71a8efc03dbec1233ec452e07fa49
URLeeo <mammadzadaaslan05@gmail.com>
2025-03-03 20:23:03 +0400

    AboutMeApp-5/added-education-repository

---
+++ src/Infrastructure/AboutMeApp.Persistence/Implementations/Repositories/EducationRepository.cs
@@ -0,0 +1,12 @@
+﻿using AboutMeApp.Application.Abstractions.Repositories;
+using AboutMeApp.Domain.Entities;
+using AboutMeApp.Persistence.Contexts;
+
+namespace AboutMeApp.Persistence.Implementations.Repositories;
+
+public class EducationRepository : Repository<Education>, IEducationRepository
+{
+    public EducationRepository(AboutMeAppDbContext context) : base(context)
+    {
+    }
+}
